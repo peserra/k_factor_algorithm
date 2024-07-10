705bfc9d9f3219c8dc849dbf85f934d79885d974
peserra <pedro.occulate@gmail.com>
2024-07-10 20:11:54 -0300

    comecando a criar g linha

--- teste.cpp
+++ teste.cpp
@@ -45,27 +45,39 @@ int encontra_indice(std::vector<int> &vetor_ordenado, int numero) {
 
 int main() {
 
-    std::vector<std::vector<int>> adjacencias;
+    std::vector<std::vector<int>> grafo_adjacencias;
+    std::vector<std::vector<int>> grafo_linha_adjacencias;
     std::vector<int> intervalos_busca;
     int soma_vizinhanca = 0;
     int k = 2;
     int indice_conversao;
 
     // grafo da folha teste
-    adjacencias.push_back(std::vector<int>{1, 2});              // 0 1 2 3
-    adjacencias.push_back(std::vector<int>{0, 2, 3});           // 4 5 6 7 8 
-    adjacencias.push_back(std::vector<int>{0, 1, 4});           // 9 10 11 12 13
-    adjacencias.push_back(std::vector<int>{1, 4, 5, 6});        // 14 15 16 17 18 19
-    adjacencias.push_back(std::vector<int>{2, 3, 6});           // 20 21 22 23 24
-    adjacencias.push_back(std::vector<int>{3, 6});              // 25 26 27 28 
-    adjacencias.push_back(std::vector<int>{3, 4, 5});           // 29 30 31 32 33
+    grafo_adjacencias.push_back(std::vector<int>{1, 2});              // 0 1 2 3
+    grafo_adjacencias.push_back(std::vector<int>{0, 2, 3});           // 4 5 6 7 8 
+    grafo_adjacencias.push_back(std::vector<int>{0, 1, 4});           // 9 10 11 12 13
+    grafo_adjacencias.push_back(std::vector<int>{1, 4, 5, 6});        // 14 15 16 17 18 19
+    grafo_adjacencias.push_back(std::vector<int>{2, 3, 6});           // 20 21 22 23 24
+    grafo_adjacencias.push_back(std::vector<int>{3, 6});              // 25 26 27 28 
+    grafo_adjacencias.push_back(std::vector<int>{3, 4, 5});           // 29 30 31 32 33
 
-    for(int i = 0; i < (int)adjacencias.size(); i++) {
+    for(int i = 0; i < (int)grafo_adjacencias.size(); i++) {
+
+        // criar tabela de busca de vertices
         indice_conversao = i * k + soma_vizinhanca;
-        soma_vizinhanca += adjacencias[i].size();
-        intervalos_busca.push_back(indice_conversao); 
-    } 
-    int indice = encontra_indice(intervalos_busca, 7); 
-    std::cout<< indice << std::endl;
+        soma_vizinhanca += grafo_adjacencias[i].size();
+        intervalos_busca.push_back(indice_conversao);
+        
+        //criar G'
+        std::vector<int> vertice_linha = {}; 
+
+        for (int i = 0; i < (int)grafo_adjacencias[i].size() + k - 1; i++){
+            vertice_linha.push_back(i + indice_conversao);
+            std::cout << i + indice_conversao << " " << std::ends;
+        } 
+        grafo_linha_adjacencias.push_back(vertice_linha);
+        std::cout << '\n' << std::ends;
+    }
+
 }
 
